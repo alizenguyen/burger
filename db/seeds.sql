4a60d51e45378e9955b6a046169eb328ffc784a4
Alize Nguyen <alizenguyen@gmail.com>
2018-01-05 15:14:08 -0600

    date base created

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac Burger');

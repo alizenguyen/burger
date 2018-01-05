bde9a53d735b323afe703ea244be4595814f687c
Alize Nguyen <alizenguyen@gmail.com>
2018-01-05 16:30:24 -0600

    server setup

--- server.js
+++ server.js
@@ -3,3 +3,28 @@ var bodyParser = require("body-parser");
 var methodOverride = require('method-override');
 
 var app = express();
+
+var PORT = process.env.PORT || 3306;
+
+//Implements bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + '/public'));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});

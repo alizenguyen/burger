f8ae58805dec82c11eb3c5335f05b02729973360
Alize Nguyen <alizenguyen@gmail.com>
2018-01-05 15:48:26 -0600

    orm added

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,74 @@
 const connection = require('../config/connection.js');
+
+//Helper Function
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+  
+    for (var key in ob) {
+      var value = ob[key];
+      if (Object.hasOwnProperty.call(ob, key)) {
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    return arr.toString();
+  }
+
+var orm = {
+    //View all from the table
+    selectAll: function (tableInput, cb) {
+        var querystring = "Select * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) throw err; 
+            cb(result);
+        });
+    },
+    //Inserting into table
+    insertOne: function (table, cols, vals, cb) {
+        var querystring = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query (queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    //Updating table
+    updateOne: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    }
+}

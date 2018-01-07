84a5f2c7c888040d06bc38675a7e17e5d5aae337
Alize Nguyen <alizenguyen@gmail.com>
2018-01-07 16:12:34 -0600

    img added

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + '/public'));
+
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
@@ -22,8 +25,6 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);

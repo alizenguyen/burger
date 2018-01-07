1db37dd5b67212921d4d46623b77996b1141f5fa
Alize Nguyen <alizenguyen@gmail.com>
2018-01-07 17:16:01 -0600

    error fixed/html edits and bootstrap edit

--- config/orm.js
+++ config/orm.js
@@ -45,7 +45,7 @@ var orm = {
         queryString += cols.toString();
         queryString += ") ";
         queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
+        queryString += "'" + vals.toString() + "'";
         queryString += ") ";
 
         console.log(queryString);

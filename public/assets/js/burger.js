62ab3b1214395e08d8eab74ccd6ee31e382c9179
Alize Nguyen <alizenguyen@gmail.com>
2018-01-07 15:37:56 -0600

    test

---
+++ public/assets/js/burger.js
@@ -0,0 +1,23 @@
+$(function () {
+    $(".add-burger").on("click", function (event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+
+        var newBurger = {
+            burger_name: $("#burg").val().trim(),
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("Created New Burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    })
+});

a3d5254b791ba0640edd108497c091b8491f154c
Alize Nguyen <alizenguyen@gmail.com>
2018-01-07 18:05:39 -0600

    small edits

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -21,5 +21,41 @@ $(function () {
                 location.reload();
             }
         );
-    })
+    });
+
+    $(".change-devoured").on("click", function (event) {
+        var id = $(this).data("id");
+        var devouredState = $(this).data("devoured");
+
+        console.log(id);
+        console.log(devouredState);
+
+        var newDevouredState = {
+            sleepy: true,
+        };
+
+        if (devouredState === 0) {
+            // Send the DELETE request.
+            $.ajax("/api/burgers/" + id, {
+                type: "PUT",
+                data: newDevourState,
+            }).then(
+                function () {
+                    console.log("Devoured burger #", id);
+                    // Reload the page to get the updated list
+                    location.reload();
+                }
+            );
+        } else if (devouredState === 1) {
+            $.ajax("/api/burgers/" + id, {
+                type: "DELETE",
+            }).then(
+                function () {
+                    console.log("Deleted burger #", id);
+                    // Reload the page to get the updated list
+                    location.reload();
+                }
+            );
+        }
+    });
 });
